19511e11287ea56e4163f6265b8b8dacbdaf17ff
Zygimantass <5236121+Zygimantass@users.noreply.github.com>
2023-05-14 01:45:48 +0200

    fix(rpc): add stats route

--- rpc/core/routes.go
+++ rpc/core/routes.go
@@ -34,7 +34,7 @@ var Routes = map[string]*rpc.RPCFunc{
 	"consensus_params":     rpc.NewRPCFunc(ConsensusParams, "height", rpc.Cacheable("height")),
 	"unconfirmed_txs":      rpc.NewRPCFunc(UnconfirmedTxs, "limit"),
 	"num_unconfirmed_txs":  rpc.NewRPCFunc(NumUnconfirmedTxs, ""),
-
+	"stats":                rpc.NewRPCFunc(Stats, ""),
 	// tx broadcast API
 	"broadcast_tx_commit": rpc.NewRPCFunc(BroadcastTxCommit, "tx"),
 	"broadcast_tx_sync":   rpc.NewRPCFunc(BroadcastTxSync, "tx"),
